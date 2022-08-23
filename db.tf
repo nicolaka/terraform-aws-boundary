18fcce2cf5af76e04529bdc9a00df602f8f5d00b
nicolaka <nicolaka@gmail.com>
2022-08-23 20:29:48 +0000

    new module v 0.0.5

--- db.tf
+++ db.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "boundary" {
   engine              = "postgres"
   engine_version      = "11.12"
   instance_class      = "db.t2.micro"
-  db_name             = "boundary-${var.name}"
+  db_name             = "boundary0${var.name}"
   username            = var.boundary_db_username
   password            = var.boundary_db_password
   skip_final_snapshot = true
